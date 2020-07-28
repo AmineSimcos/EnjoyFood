4910df9fd5b45b517e5c4c246848cf67cfa89de3
amine <aminebaich116@gmail.com>
2020-07-28 19:44:08 +0100

    misea jour 3

--- app/src/main/java/com/exemple/enjoyfood/ui/MainActivity.java
+++ app/src/main/java/com/exemple/enjoyfood/ui/MainActivity.java
@@ -86,7 +86,7 @@ public class MainActivity extends AppCompatActivity{
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_accueil, R.id.nav_produit, R.id.nav_consommation, R.id.nav_historique, R.id.nav_apropos, R.id.nav_profile)
+                R.id.nav_accueil, R.id.nav_produit, R.id.nav_consommation, R.id.nav_historique, R.id.nav_apropos)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
